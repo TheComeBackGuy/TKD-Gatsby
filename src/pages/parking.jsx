8dab5b62d27fc578d64141d247e0c7b2d77b2fda
TheComeBackGuy <dennis.hart@gmail.com>
2021-11-07 07:13:13 -0500

    about to start adding the blog element

--- src/pages/parking.jsx
+++ src/pages/parking.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import { StaticImage } from 'gatsby-plugin-image'
 import './styles/pages.css'
+import './styles/parking.css'
 
 export default function Parking() {
     return (
@@ -10,7 +12,54 @@ export default function Parking() {
                 <Header text="This is the header" />
             </header>
             <main className="container">
-                <h1 className="standardPage"> This is the Parking Page.</h1>
+                <h1 className="standardPage">Parking</h1>
+                <div className="card">
+                    <h3>We are located at:</h3>
+                    <cite>
+                        800 S. Central Street
+                        <br />
+                        Knoxville, TN 37902
+                    </cite>
+
+                    <h2>Parking options</h2>
+                    <div className="parking">
+                        <p className="knockout metered">
+                            Metered parking is available on all of the streets
+                            around the theater. The meters are FREE evenings
+                            after 6 pm and all day Sunday. Parking is also
+                            available in several garages nearby.
+                        </p>
+                        <div className="knockout">
+                            <h3>State Street Garage</h3>
+                            <StaticImage
+                                className="parkingImage"
+                                src="../images/state-street-parking.jpg"
+                                alt="State Street Parking"
+                                description="A multi-level parking building"
+                            />
+
+                            <p>
+                                YO! State Street garage, 2 blocks north of the
+                                theater on S. Central St. This garage is FREE
+                                evenings and weekends.
+                            </p>
+                        </div>
+                        <div className="knockout">
+                            <h3>First Horizon Bank Garage</h3>
+                            <StaticImage
+                                className="parkingImage"
+                                src="../images/first-horizon-bank.jpg"
+                                alt="First Horizon Bank"
+                                description="A tall building in the middle of downtown Knoxville"
+                            />
+                            <p>
+                                First Horizon Bank garage, 1 block west of the
+                                theater on Cumberland Ave. This garage offers
+                                discount rates on evenings and weekends.
+                            </p>
+                        </div>
+                    </div>
+                </div>
                 <Footer />
             </main>
         </body>
--- src/pages/theBoard.jsx
+++ src/pages/theBoard.jsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import './styles/pages.css'
+import './styles/theBoard.css'
 
 export default function TheBoard() {
     return (
@@ -12,6 +13,28 @@ export default function TheBoard() {
             </header>
             <main className="container">
                 <h1 className="standardPage">Board of Directors</h1>
+                <div className="card">
+                    <ul className="boardNames">
+                        <li>
+                            <h1>President: Sarah Campbell</h1>
+                        </li>
+                        <li>
+                            <h1>Vice-President: Joe Johnson</h1>
+                        </li>
+                        <li>
+                            <h2>Secretary: Courtney Woolard </h2>
+                        </li>
+                        <li>
+                            <h2>Treasurer: Windie Wilson</h2>
+                        </li>
+                        <li>
+                            <h3>Dennis Hart</h3>
+                        </li>
+                        <li>
+                            <h3>Arisa Carero Spencer</h3>
+                        </li>
+                    </ul>
+                </div>
                 <Footer />
             </main>
         </body>
