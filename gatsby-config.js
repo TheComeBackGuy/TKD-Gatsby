9bc67c381080eaf86b2868846aaa430df9ee52b4
TheComeBackGuy <dennis.hart@gmail.com>
2021-11-07 10:31:53 -0500

    Added files and shows to json file

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,7 @@ module.exports = {
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: `${__dirname}/src/images`,
-            },
-        },
+
         {
             resolve: `gatsby-plugin-google-fonts`,
             options: {
--- src/pages/currentSeason.jsx
+++ src/pages/currentSeason.jsx
@@ -2,16 +2,68 @@ import * as React from 'react'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
-
+import JSONData from '../content/thisYear.json'
+import './styles/currentSeason.css'
 import './styles/pages.css'
-export default function Index() {
+// import { StaticImage } from 'gatsby-plugin-image'
+
+export default function CurrentSeason() {
     return (
         <body className="everything">
             <header>
                 <Header text="This is the header" />
             </header>
+            {/* <ul>
+                {JSONData.content.map((data, index) => {
+                    return <li key={`content_item_${index}`}>{data.item}</li>
+                })}
+            </ul> */}
             <main className="container">
                 <h1 className="standardPage">2021/22 SEASON</h1>
+                <div className="card">
+                    {/* <h1>{JSONData[0].title}</h1> */}
+                    <ul>
+                        {JSONData.map((data, index) => {
+                            return (
+                                <li key={`content_item_${index}`}>
+                                    <div className="knockoutWhite">
+                                        <h3>{data.title}</h3>
+                                        <p>
+                                            <cite>By: {data.author}</cite>
+                                            <br />
+                                            Opens: {data.opens}
+                                            <br />
+                                            Closes: {data.closes}
+                                            <br />
+                                            Directed By: {data.director}
+                                        </p>
+                                        <button
+                                            className="tickets"
+                                            onClick={() => {
+                                                window.open(
+                                                    `https://www.onthestage.tickets/show/theatre-knoxville-downtown/${data.ticketLink}/tickets`
+                                                )
+                                            }}
+                                        >
+                                            Tickets
+                                        </button>
+                                        <ul>
+                                            {data.cast.map((role) => {
+                                                return (
+                                                    <li>
+                                                        {' '}
+                                                        {role.name}:{' '}
+                                                        {role.actor}
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
                 <Footer />
             </main>
         </body>
