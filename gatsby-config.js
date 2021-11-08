91e77d9046ef90ec7410370a650a42470abfd867
TheComeBackGuy <dennis.hart@gmail.com>
2021-11-07 19:44:13 -0500

    deleted gatsby-source-filedata as it was throwing errors.

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,6 @@ module.exports = {
                 path: `${__dirname}/src/pages/`,
             },
         },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `data`,
-                path: `${__dirname}/src/data/`,
-                ignore: [`**/\.*`], // ignore files starting with a dot
-            },
-        },
 
         {
             resolve: `gatsby-plugin-google-fonts`,
