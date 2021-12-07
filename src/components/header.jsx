8ac129229143cf1c954bbd833106ea564742890f
TheComeBackGuy <dennis.hart@gmail.com>
2021-12-07 16:19:33 -0500

    cleared cache

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -16,31 +16,31 @@ export default function Header(props) {
     /** We're detecting if the user scrolled down and if we're in desktop veiw
      * If they scroll dow, we shrink the navbar.
      */
-    window.onscroll = () => {
-        if (window.scrollY >= 100 && window.innerWidth >= 850) {
-            document.body.querySelector('.headerGrid').classList.add('shrink')
-            document.body.querySelector('.logo').classList.add('logoShrink')
-            /**If they scroll back to the top, we make the bar the original size
-             * by deleting the style we added.
-             */
-        } else if (window.scrollY < 100 && window.innerWidth >= 850) {
-            document.body
-                .querySelector('.headerGrid')
-                .classList.remove('shrink')
-            document.body.querySelector('.logo').classList.remove('logoShrink')
-            /**but if the window is smaller than the size our hamburger menu
-             * kicks in, it doesn't shrink anything. It makes sure to remove the style
-             * as soon as it loads in.
-             */
-        } else if (window.innerWidth < 850) {
-            document.body
-                .querySelector('.headerGrid')
-                .classList.remove('shrink')
-            document.body.querySelector('.logo').classList.remove('logoShrink')
-        }
+    // window.onscroll = () => {
+    //     if (window.scrollY >= 100 && window.innerWidth >= 850) {
+    //         document.body.querySelector('.headerGrid').classList.add('shrink')
+    //         document.body.querySelector('.logo').classList.add('logoShrink')
+    //         /**If they scroll back to the top, we make the bar the original size
+    //          * by deleting the style we added.
+    //          */
+    //     } else if (window.scrollY < 100 && window.innerWidth >= 850) {
+    //         document.body
+    //             .querySelector('.headerGrid')
+    //             .classList.remove('shrink')
+    //         document.body.querySelector('.logo').classList.remove('logoShrink')
+    //         /**but if the window is smaller than the size our hamburger menu
+    //          * kicks in, it doesn't shrink anything. It makes sure to remove the style
+    //          * as soon as it loads in.
+    //          */
+    //     } else if (window.innerWidth < 850) {
+    //         document.body
+    //             .querySelector('.headerGrid')
+    //             .classList.remove('shrink')
+    //         document.body.querySelector('.logo').classList.remove('logoShrink')
+    //     }
 
-        // console.log(window.scrollY)
-    }
+    //     // console.log(window.scrollY)
+    // }
 
     return (
         <header className="headerConstraint">
